297b9d42d85044da89d412cf61ae1f800414c479
Andrea To Chip <andreatochip@yahoo.com>
2026-03-05 10:13:05 +0800

    Update App.js

--- App.js
+++ App.js
@@ -29,3 +29,22 @@ FUNCTION handle_form_submission(form_data):
 
 END FUNCTION
 */
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
